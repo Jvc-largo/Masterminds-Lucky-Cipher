4aef3aaa1a4b44ae4cf49e6d3ad1de25affe4e74
Jvc-largo <largovirgil@gmail.com>
2026-03-06 18:27:40 -0800

    Added Ciphera Scripts to Outer Folder

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Random answer generation
+    public Sprite[] optionSprites;
+    public Sprite[] answerSequence;
+    // Clicking logic
+    public Transform[] guessSlots;
+    public Sprite[] playerGuess;
+
+    private int currentSlot = 0;
+
+    void Start()
+    {
+        answerSequence = new Sprite[guessSlots.Length];
+        playerGuess = new Sprite[guessSlots.Length];
+
+        for (int i = 0; i < answerSequence.Length; i++)
+        {
+            answerSequence[i] = optionSprites[Random.Range(0, optionSprites.Length)];
+        }
+
+        // For debugging, print the answer
+        string answerStr = "";
+        foreach (var sprite in answerSequence)
+        {
+            answerStr += sprite.name + " ";
+        }
+
+        Debug.Log("Answer sequence: " + answerStr);
+    }
+
+    public void PlaceObject(Sprite selectedSprite)
+    {
+        if (currentSlot >= guessSlots.Length)
+        {
+            Debug.Log("Guess row full");
+            return;
+        }
+
+        GameObject newObject = new GameObject("GuessItem");
+
+        SpriteRenderer sr = newObject.AddComponent<SpriteRenderer>();
+        sr.sprite = selectedSprite;
+
+        newObject.transform.position = guessSlots[currentSlot].position;
+        playerGuess[currentSlot] = selectedSprite;
+
+        currentSlot++;
+    }
+}
